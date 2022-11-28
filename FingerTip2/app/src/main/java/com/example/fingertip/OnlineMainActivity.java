c87052afcb9f7f44a91cc9f4fd3a2fee642c6011
parkheesoo <qkrgmltn1357@naver.com>
2022-11-28 15:40:06 +0900

    인터넷 퍼미션 문제등 수정

--- FingerTip2/app/src/main/java/com/example/fingertip/OnlineMainActivity.java
+++ FingerTip2/app/src/main/java/com/example/fingertip/OnlineMainActivity.java
@@ -2,7 +2,9 @@ package com.example.fingertip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OnlineMainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class OnlineMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_main);
+    }
+
+    public void goOnlineShoppingMallActivity(View view){
+        Intent intent = new Intent(OnlineMainActivity.this, OnlineMallActivity.class);
+        startActivity(intent);
     }
 }
