59e834c515a7c6c51f6838d4c7433fdd4e582c47
parkheesoo <qkrgmltn1357@naver.com>
2022-12-07 18:22:32 +0900

    fab button

--- FingerTip2/app/src/main/java/com/example/fingertip/MainActivity.java
+++ FingerTip2/app/src/main/java/com/example/fingertip/MainActivity.java
@@ -4,13 +4,22 @@ import static android.speech.tts.TextToSpeech.ERROR;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,20 +28,32 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context cThis;//context 설정
     private TextToSpeech tts;
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
     private TextView tv_current_user;
     private String current_user_nickname;
+    private FloatingActionButton fab;
+    Intent SttIntent;
+    SpeechRecognizer mRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        cThis=this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SttIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        SttIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getApplicationContext().getPackageName());
+        SttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");//한국어 사용
+        mRecognizer= SpeechRecognizer.createSpeechRecognizer(cThis);
+        mRecognizer.setRecognitionListener(listener);
 
         tv_current_user = findViewById(R.id.username_tv);
 
@@ -40,6 +61,23 @@ public class MainActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference("FingerTip");
 
         FirebaseUser firebaseUser = mAuth.getCurrentUser();
+
+        fab = (FloatingActionButton)findViewById(R.id.fab_btn);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(cThis, Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.RECORD_AUDIO},1);
+                    //권한을 허용하지 않는 경우
+                }else{
+                    //권한을 허용한 경우
+                    try {
+                        mRecognizer.startListening(SttIntent);
+                    }catch (SecurityException e){e.printStackTrace();}
+                }
+            }
+        });
 
         mDatabase.child("UserAccount").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
@@ -60,6 +98,60 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private RecognitionListener listener=new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            //txtSystem.setText("onReadyForSpeech..........."+"\r\n"+txtSystem.getText());
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            //txtSystem.setText("지금부터 말을 해주세요..........."+"\r\n"+txtSystem.getText());
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+            //txtSystem.setText("onBufferReceived..........."+"\r\n"+txtSystem.getText());
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            //txtSystem.setText("onEndOfSpeech..........."+"\r\n"+txtSystem.getText());
+        }
+
+        @Override
+        public void onError(int i) {
+            //txtSystem.setText("천천히 다시 말해 주세요..........."+"\r\n"+txtSystem.getText());
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            String key= "";
+            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult =results.getStringArrayList(key);
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+            //txtInMsg.setText(rs[0]+"\r\n"+txtInMsg.getText());
+            //FuncVoiceOrderCheck(rs[0]);
+            mRecognizer.startListening(SttIntent);
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+            //txtSystem.setText("onPartialResults..........."+"\r\n"+txtSystem.getText());
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+            //txtSystem.setText("onEvent..........."+"\r\n"+txtSystem.getText());
+        }
+    };
 
     public void setTts(){
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
